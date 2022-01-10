76e972a6132e4108f2f7f4cd0401bde764561be2
manel talbi <talbimanel20@gmail.com>
2022-01-10 10:40:02 +0100

    write to a text file c#

--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace AssembliesAndNamespaces
+﻿using System;
+using System.IO;
+namespace AssembliesAndNamespaces
 {
     class program
     {
         static void Main(string[] args)
         {
+            string text =
+            "A class is the most powerful data type in C#. Like a structure, " +
+            "a class defines the data and behavior of the data type. ";
+
+            File.WriteAllText("C:/lesson 17/WriteText.txt", text);
+
+
             Console.WriteLine("Hello, World!");
             Console.ReadLine();
         }
