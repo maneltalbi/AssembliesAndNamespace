88a8423c10b64f8c7075a568162c3a1ee7f6944b
manel talbi <talbimanel20@gmail.com>
2022-01-11 09:44:51 +0100

    modifie text file

--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -6,11 +6,9 @@ namespace AssembliesAndNamespaces
     {
         static void Main(string[] args)
         {
-            string text =
-            "A class is the most powerful data type in C#. Like a structure, " +
-            "a class defines the data and behavior of the data type. ";
-
-            File.WriteAllText("C:/lesson 17/WriteText.txt", text);
+            string text = "We want to write this to our file.";
+            
+            File.WriteAllText(@"C:/lesson 17/WriteText.txt", text);
 
 
             Console.WriteLine("Hello, World!");
